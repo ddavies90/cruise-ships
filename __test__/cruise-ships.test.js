133f0bc6bb6280778933a06017ee8c88f1073fdf
David Davies <d.davies90@hotmail.co.uk>
2021-05-22 12:02:59 +0100

    Added port object constructor with tests. Also added setSail method on to ship prototype.

--- __test__/cruise-ships.test.js
+++ __test__/cruise-ships.test.js
@@ -1,15 +1,17 @@
 const {Ship} = require("../src/cruise-ships")
 
 let ship;
+let port;
 
 beforeEach(() => {
     ship = new Ship('Yokohama');
+    port = new Port('Yokohama');
 });
 
 // As a cruise ship captain,
 // So I can get passengers aboard a ship,
 // I want a ship to have a starting port.
-describe('constructor', () => {
+describe('Ship constructor', () => {
     it("Creates new instance of the Ship object", () => {
         expect(ship).toBeInstanceOf(Ship);
     });
@@ -17,14 +19,22 @@ describe('constructor', () => {
     it("Contains starting port equal to the value passed in as argument", () => {
         expect(ship).toEqual(expect.objectContaining({startingPort: 'Yokohama'}));
     });
-});
 
 //Getting passengers aboard ?how will this work ?Counter or array of people <- probably better to be array but will passenger also be object? or just name
 
-describe('boardPassenger', () => {
     it("Adds passenger to ship's passenger array", () => {
         ship.boardPassenger('Jeff');
 
         expect(ship.passengers).toEqual(["Jeff"]);
+    });
+
+
+// As a cruise ship captain,
+// So I can get passengers started on their journey,
+// I want a ship to be able to set sail from a port.
+    it("Sets sail from dock", () => {
+        ship.setSail();
+
+        expect(ship.isDocked).toEqual(false);
     });
 });
--- src/cruise-ships.js
+++ src/cruise-ships.js
@@ -1,13 +1,22 @@
 
-function Ship (startingPort) {
+function Ship(startingPort) {
     this.startingPort = startingPort;
     this.passengers = [];
+    this.isDocked = true;
 };
 
 Ship.prototype = {
     boardPassenger(passenger) {
         this.passengers.push(passenger);
     },
-}
+    setSail() {
+        this.isDocked = false;
+    },
 
-module.exports = {Ship}
+};
+
+function Port(name) {
+    this.name = name;
+};
+
+module.exports = {Ship, Port}
