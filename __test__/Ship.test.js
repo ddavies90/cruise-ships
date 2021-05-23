c3dc81e9610c4a96c6537124e89e411221dcd542
David Davies <d.davies90@hotmail.co.uk>
2021-05-23 22:21:29 +0100

    Added tests for dock method and updated tests for the constructor to take the port  object as the argument instead of a string

--- __test__/Ship.test.js
+++ __test__/Ship.test.js
@@ -1,56 +1,65 @@
-const {Ship} = require("../src/cruise-ships")
+const { TestScheduler } = require("@jest/core");
+const {Ship, Port} = require("../src/cruise-ships")
 
 let ship;
+let port;
 
 beforeEach(() => {
-    ship = new Ship('Yokohama');
+    port = new Port('Yokohama')
+    ship = new Ship(port);
 });
 
 // As a cruise ship captain,
 // So I can get passengers aboard a ship,
 // I want a ship to have a starting port.
 describe('constructor', () => {
-    it("Creates new instance of the Ship object", () => {
+    it('Creates new instance of the Ship object', () => {
         expect(ship).toBeInstanceOf(Ship);
     });
-    it("Contains starting port equal to the value passed in as argument", () => {
+    it('Contains starting port equal to the name property of the object passed in as argument', () => {
         expect(ship).toEqual(expect.objectContaining({startingPort: 'Yokohama'}));
     });
-    it('Takes a string as an argument only', () => {
+    it('Throws an error if it does not receive an object as argument', () => {
         expect(() => {
-            let ship2 = new Ship(9)
-        }).toThrow('Please enter a valid port name');
+            const ship2 = new Ship('JLB Credit');
+        }).toThrow('Please pass in a valid port object')
+    });
+    it('Throws an error if object does not contain a name property', () => {
+        const port2 = {age: 3};
+        expect(() => {
+            const ship2 = new Ship(port2);
+        }).toThrow('Please pass in a valid port object');
     });
 });
 
 //Getting passengers aboard ?how will this work ?Counter or array of people <- probably better to be array but will passenger also be object? or just name
 describe('boardPassenger', () => {
     it("Adds passenger to ship's passenger array", () => {
-        ship.boardPassenger('Lando Norris');
+        ship.boardPassenger = 'Lando Norris';
 
         expect(ship.passengers).toEqual(['Lando Norris']);
     });
-    it('Throws an error if no argument is passed in', () => {
+    it('Throws an error if empty string is passed in', () => {
         expect(() => {
-            ship.boardPassenger();
+            ship.boardPassenger = '';
         }).toThrow('Please enter a name to use this function');
     });
     it('Throws an error if a name containing < 2 letters is passed', () => {
         expect(() => {
-            ship.boardPassenger('J');
+            ship.boardPassenger = 'J';
         }).toThrow('Please enter a name to use this function');
     });
     it('Throws an error is a number is passed in as a name, even if it is a string', () => {
         expect(() => {
-            ship.boardPassenger(888);
+            ship.boardPassenger = '888';
         }).toThrow('Please enter a name to use this function');
         expect(() => {
-            ship.boardPassenger(8);
+            ship.boardPassenger = 8;
         }).toThrow('Please enter a name to use this function');
     });
-    it('Throws an error if there a number anywhere in the argument', () => {
+    it('Throws an error if there a number anywhere in the passed string', () => {
         expect(() => {
-            ship.boardPassenger('f7643');
+            ship.boardPassenger = 'f7643';
         }).toThrow('Please enter a name to use this function');
     });
 });
@@ -63,4 +72,13 @@ describe('setSail', () => {
         ship.setSail();
         expect(ship.isDocked).toBe(false);
     });
-})
+});
+
+describe('dock', () => {
+    it('Changes startingPort to value of name property of new port', () => {
+        const port2 = new Port('Ishinomaki');
+        ship.dock = port2;
+
+        expect(ship.startingPort).toBe('Ishinomaki');
+    });
+});
