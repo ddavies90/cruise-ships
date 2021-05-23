c370cc6b7a45ac29e7b647e8b2f47d62da76eba3
David Davies <d.davies90@hotmail.co.uk>
2021-05-23 22:57:40 +0100

    Added test to check that port object contains a name property

--- __test__/Port.test.js
+++ __test__/Port.test.js
@@ -10,6 +10,9 @@ describe('constructor', () => {
     it('Creates new instance of the Port object', () => {
         expect(port).toBeInstanceOf(Port);
     });
+    it('contains name property', () => {
+        expect(port.hasOwnProperty('name')).toEqual(true);
+    });
     it('Takes a string as an argument only', () => {
         expect(() => {
             const port2 = new Port(8382)
