66ca46e6e8c78d06b8fa9866122e36f4f406bf5e
David Davies <d.davies90@hotmail.co.uk>
2021-05-23 22:47:44 +0100

    Added dock method to ship class. Changed boardPassenger to be a setter method. Added logic to check that Ship construcor only accepts Port object as argument

--- src/cruise-ships.js
+++ src/cruise-ships.js
@@ -1,33 +1,37 @@
 
 class Ship {
     constructor(startingPort) {
-        if(typeof(startingPort) !== 'string') {
-            throw new Error('Please enter a valid port name');
-        } else {
-            this.startingPort = startingPort;
+        if (!(startingPort instanceof Port)) {
+            throw new Error('Please pass in a valid port object')
         };
+        this.startingPort = startingPort.name;
         this.passengers = [];
         this.isDocked = true;
-    }
-    boardPassenger(passenger) {
+    };
+    set boardPassenger(passenger) {
         if (!passenger || typeof(passenger) !== 'string' || passenger.length < 2 || !isNaN(parseInt(passenger)) || (/\d/).test(passenger)) {
             throw new Error('Please enter a name to use this function')
         } else {
         this.passengers.push(passenger);
         };
-    }
+    };
     setSail() {
         this.isDocked = false;
-    }
+    };
+    set dock(portName) {
+        if (!(portName instanceof Port)) {
+            throw new Error('Please pass in a valid port object')
+        };
+        this.startingPort = portName.name;
+    };
 };
 
 class Port {
     constructor(name) {
-        if(typeof(name) !== 'string') {
+        if(!name || typeof(name) !== 'string') {
             throw new Error('Please enter a valid port name');
-        } else {
-            this.name = name;
         };
+            this.name = name;
     }
 }
 
