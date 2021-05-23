b81765bff976a8d990ec1401fb7112698a09140a
David Davies <d.davies90@hotmail.co.uk>
2021-05-23 21:27:59 +0100

    Changed constructor method in to class for Ship. Added logic for checking argument passed in to the ship constructor and the boardPassenger method. Added Port class and constructor method.

--- src/cruise-ships.js
+++ src/cruise-ships.js
@@ -1,22 +1,35 @@
 
-function Ship(startingPort) {
-    this.startingPort = startingPort;
-    this.passengers = [];
-    this.isDocked = true;
-};
-
-Ship.prototype = {
+class Ship {
+    constructor(startingPort) {
+        if(typeof(startingPort) !== 'string') {
+            throw new Error('Please enter a valid port name');
+        } else {
+            this.startingPort = startingPort;
+        };
+        this.passengers = [];
+        this.isDocked = true;
+    }
     boardPassenger(passenger) {
+        if (!passenger || typeof(passenger) !== 'string' || passenger.length < 2 || !isNaN(parseInt(passenger)) || (/\d/).test(passenger)) {
+            throw new Error('Please enter a name to use this function')
+        } else {
         this.passengers.push(passenger);
-    },
+        };
+    }
     setSail() {
         this.isDocked = false;
-    },
-
+    }
 };
 
-function Port(name) {
-    this.name = name;
-};
+class Port {
+    constructor(name) {
+        if(typeof(name) !== 'string') {
+            throw new Error('Please enter a valid port name');
+        } else {
+            this.name = name;
+        };
+    }
+}
+
 
 module.exports = {Ship, Port}
