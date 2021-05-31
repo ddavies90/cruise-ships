83d92b877e16187a7f3649193f1391434e40dc42
David Davies <d.davies90@hotmail.co.uk>
2021-05-31 23:16:55 +0100

    Removed comment

--- src/itinerary.js
+++ src/itinerary.js
@@ -1,5 +1,4 @@
 class Itinerary {
-    //I would like this to take individual port objects and push them to the array rather than have it accept an array ?use getter and setter functions
     constructor(ports) {
         this.ports = ports;
     };
