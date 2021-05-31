662376b18d07cdb1f9edb6cf9b709569e0027b3d
David Davies <d.davies90@hotmail.co.uk>
2021-05-31 23:02:48 +0100

    Added addShip and removeShip methods to class and tidied up test set up"

--- src/port.js
+++ src/port.js
@@ -4,6 +4,13 @@ class Port {
             throw new Error('Please enter a valid port name');
         };
             this.name = name;
+            this.ships = [];
+    };
+    addShip(ship) {
+        this.ships.push(ship);
+    };
+    removeShip(ship) {
+        this.ships = this.ships.filter(item => item !== ship)
     };
 };
 
