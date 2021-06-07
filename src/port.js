35fb7a6e824221f774f5e9ad12980fe74d600bef
David Davies <d.davies90@hotmail.co.uk>
2021-06-07 08:02:52 +0100

    Changed class construction to be wrapped inside IIFEs so they can be run in both the browser and node environments

--- src/port.js
+++ src/port.js
@@ -1,17 +1,22 @@
-class Port {
-    constructor(name) {
-        if(!name || typeof(name) !== 'string') {
-            throw new Error('Please enter a valid port name');
+(function exportPort() {
+    class Port {
+        constructor(name) {
+            if(!name || typeof(name) !== 'string') {
+                throw new Error('Please enter a valid port name');
+            };
+                this.name = name;
+                this.ships = [];
         };
-            this.name = name;
-            this.ships = [];
+        addShip(ship) {
+            this.ships.push(ship);
+        };
+        removeShip(ship) {
+            this.ships = this.ships.filter(item => item !== ship)
+        };
     };
-    addShip(ship) {
-        this.ships.push(ship);
-    };
-    removeShip(ship) {
-        this.ships = this.ships.filter(item => item !== ship)
-    };
-};
-
-module.exports = Port;
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Port;
+    } else {
+        window.Port = Port;
+    }
+}());
--- src/ship.js
+++ src/ship.js
@@ -1,44 +1,47 @@
-class Ship {
-    constructor(name, itinerary) {
-        if (!name || !itinerary || typeof name !== 'string' || typeof itinerary !== 'object') {
-            throw new Error('Please pass in valid arguments')
-        };
-        this.name = name;
-        this.currentPort = itinerary.ports[0];
-        this.itinerary = itinerary;
-        this.passengers = [];
-        this.previousPort = null;
-        this.currentPort.addShip(this);
-    };
-    set boardPassenger(passenger) {
-        if (!passenger || typeof passenger !== 'string' || passenger.length < 2 || !isNaN(parseInt(passenger)) || (/\d/).test(passenger)) {
-            throw new Error('Please enter a valid name')
-        } else {
-        this.passengers.push(passenger);
-        };
-    };
-    setSail() {
-        const currentPortIndex = this.itinerary.ports.indexOf(this.currentPort);
-        if (this.itinerary.ports[currentPortIndex + 1] !== undefined) {
-            this.previousPort = this.currentPort;
-            this.previousPort.removeShip(this);
-            this.currentPort = null;
-        } else {
-            throw new Error('The ship can not leave the dock without a destination');
-        };
-    };
-    dock() {
-        const prevPortIndex = this.itinerary.ports.indexOf(this.previousPort);
-        //If not docked
-        if (!(this.currentPort)) {
-            this.currentPort = this.itinerary.ports[prevPortIndex + 1];
+(function exportShip() {
+    class Ship {
+        constructor(name, itinerary) {
+            if (!name || !itinerary || typeof name !== 'string' || typeof itinerary !== 'object') {
+                throw new Error('Please pass in valid arguments')
+            };
+            this.name = name;
+            this.currentPort = itinerary.ports[0];
+            this.itinerary = itinerary;
+            this.passengers = [];
+            this.previousPort = null;
             this.currentPort.addShip(this);
-        } else {
-            throw new Error('The ship is already docked');
+        };
+        set boardPassenger(passenger) {
+            if (!passenger || typeof passenger !== 'string' || passenger.length < 2 || !isNaN(parseInt(passenger)) || (/\d/).test(passenger)) {
+                throw new Error('Please enter a valid name')
+            } else {
+            this.passengers.push(passenger);
+            };
+        };
+        setSail() {
+            const currentPortIndex = this.itinerary.ports.indexOf(this.currentPort);
+            if (this.itinerary.ports[currentPortIndex + 1] !== undefined) {
+                this.previousPort = this.currentPort;
+                this.previousPort.removeShip(this);
+                this.currentPort = null;
+            } else {
+                throw new Error('The ship can not leave the dock without a destination');
+            };
+        };
+        dock() {
+            const prevPortIndex = this.itinerary.ports.indexOf(this.previousPort);
+            //If not docked
+            if (!(this.currentPort)) {
+                this.currentPort = this.itinerary.ports[prevPortIndex + 1];
+                this.currentPort.addShip(this);
+            } else {
+                throw new Error('The ship is already docked');
+            };
         };
     };
-};
-
-
-
-module.exports = Ship;
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Ship;
+    } else {
+        window.Port = Ship;
+    }
+}());
